e4e30ae956cd0ae25c31c39f43de4bd5ac46d6e6
jockrow <thecrowr@hotmail.com>
2020-12-22 08:37:01 -0500

    method POST with parameter

--- src/App.tsx
+++ src/App.tsx
@@ -14,22 +14,30 @@ function App() {
  useEffect(() => {
     setAppState({ loading: true, repos: null });
 
-	const apiUrl = 'https://search.torre.co/people/_search/?currency=USD%24&page=0&periodicity=hourly&lang=es&size=20&aggregate=false&offset=0';
+     let body = { name: {term: 'Richard Martínez'} };
+    const apiUrl = 'https://search.torre.co/people/_search/?currency=USD%24&page=0&periodicity=hourly&lang=es&size=20&aggregate=false&offset=0';
 
-	 const requestOptions = {
-		method: 'POST',
-		headers: { 
-			'Content-Type': 'application/json',
+     const requestOptions = {
+        method: 'POST',
+        headers: { 
 			'Authorization': 'Bearer my-token',
-			'My-Custom-Header': 'foobar'
-		},
-		body: JSON.stringify({ title: 'React POST Request Example' })
-	};
-	fetch(apiUrl, requestOptions)
-		.then((res) => res.json())
-	  .then((repos) => {
-			console.log(repos.results);
-			setAppState({ loading: false, repos: repos.results });
+			'My-Custom-Header': 'foobar',
+			'accept': 'application/json, text/plain, */*',
+			'accept-language': 'es-419,es;q=0.9,es-ES;q=0.8,en;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+			'content-type': 'application/json;charset=UTF-8',
+			'sec-fetch-dest': 'empty',
+			'sec-fetch-mode': 'cors',
+			'sec-fetch-site': 'same-site',
+			'x-torre-subject': '506501'
+        },
+         body: JSON.stringify(body)
+    };
+    fetch(apiUrl, requestOptions)
+        .then((res) => res.json())
+      .then((repos) => {
+            console.log(repos.results);
+            //console.log(repos.results);
+            setAppState({ loading: false, repos: repos.results });
 	  });
   }, [setAppState]);
 
